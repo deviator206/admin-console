243e32f86e7d627dec1db44e6c8d87d2f2e27369
tputtewar <tmanchalwar@gmail.com>
2018-09-30 03:16:06 +0530

    changes for taking data from rest and successfully login.

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -5,9 +5,19 @@ class LoginComponent extends Component {
 
     constructor(props, context) {
         super(props);
+        this.state = {
+            username: '',
+            password: '',
+            data: '',
+            authenticated:false,
+          };
         this.context = context;
         this.onSubmitHandler= this.onSubmitHandler.bind(this);
         this.onLoginSuccessCallback = this.onLoginSuccessCallback.bind(this);
+        this.handlePassChange = this.handlePassChange.bind(this);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.props = props;
+        
     }
 
     componentWillMount(){
@@ -18,42 +28,85 @@ class LoginComponent extends Component {
         console.log(newProps);
     }
 
+  
 
-    onLoginSuccessCallback (response) {
+    onLoginSuccessCallback =(response) => {
         this.props.onLoginSuccess({authenticated:true});
        this.props.history.push("/dashboard")
     }
-    onSubmitHandler() {
-    	console.log('inputUserName :: '+this.state.inputUserName);
-    	    	console.log('inputPassword :: '+this.state.inputPassword);
-    	    	console.log('URL ::  http://localhost:8080/http://localhost:8080/login/username/'+this.state.inputUserName);
-    	    	 if (!this.state.inputUserName) {
-    	    	      return this.setState({ error: 'Username is required' });
-    	    	    }
-    	
-    	    	    if (!this.state.inputPassword) {
-    	    	      return this.setState({ error: 'Password is required' });
-    	    	    }
-    	    	fetch('http://localhost:8080/http://localhost:8080/login/username/'+this.state.inputUserName)
-    	        .then(response => response.json())
-    	        .then(data => this.setState(data),this.onLoginSuccessCallback);
-    	      /*  AppService.invokeLogin({
-    	            "name": "",
-    	            "job": "leader"
-    	        },
-    	        this.onLoginSuccessCallback);*/
-    }
 
+    onLoginFailureCallback (error) {
+        console.log(error);
+        this.props.onLoginFailure({authenticated:false});
+       this.props.history.push("/")
+    }
+    onSubmitHandler=() => {
+        
+        var url = 'http://localhost:8080/login/username/'+this.state.username;
+        console.log(url);
+        this.setState({ authenticated: false });
+        fetch(url)
+        .then(response => {
+            if (response.ok) {
+                
+              return response.json();
+            } else {
+              throw new Error('Something went wrong ...');
+              
+            }
+        })
+        .then(data => {
+            
+           if(this.state.username===data.username && this.state.password===data.password){
+            console.log(data.username +':: Authenticated');
+            this.props.onLoginSuccess({authenticated:true});
+            this.props.history.push("/dashboard")
+           }else{
+            console.log(data.username +':: Not Authenticated');
+            this.props.onLoginSuccess({authenticated:false});
+            this.props.history.push("/")
+           }
+        })
+        .catch(error => {
+            console.log('Something is wrong with something!','error');
+            this.setState({ error:error, authenticated: false })
+            this.props.onLoginSuccess({authenticated:false});
+                this.props.history.push("/")
+        });
+
+       
+    }
+    
+    resetValues(evt){
+        if(evt.target.id==='inputUserName'){
+            evt.target.value ='';  
+        }else  if(evt.target.id==='inputPassword'){
+            evt.target.value ='';  
+        }
+    }
+    handleUserChange(evt) {
+        this.setState({
+            username: evt.target.value,
+        });
+      };
+    
+      handlePassChange(evt) {
+        this.setState({
+            password: evt.target.value,
+        });
+      }
+    
     render() {
+        
         return (
             <div className="container text-center">
                 <div className="form-signin">
                     <img className="mb-4" src="css/img/icons8-login-64.png" alt="" width="72" height="72"></img>
                     <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                     <label htmlFor="inputUserName" className="sr-only">User Name</label>
-                    <input type="text" id="inputUserName" className="form-control" placeholder="User Name" required="" autoFocus=""></input>
+                    <input type="text" id="inputUserName" className="form-control"  placeholder="User Name"  onChange={this.handleUserChange} required autoFocus></input>
                     <label htmlFor="inputPassword" className="sr-only">Password</label>
-                    <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""></input>
+                    <input type="password" id="inputPassword" className="form-control" placeholder="Password"  onChange={this.handlePassChange} required></input>
                     <button className="btn btn-lg btn-primary btn-block" type="submit" onClick={this.onSubmitHandler}>Sign in</button>
                 </div>
             </div>
